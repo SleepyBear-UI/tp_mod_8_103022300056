33654b46e2714f70fc73fa281ae6655de66942bd
Daffa Khairi Putra Fadli_ 103022300056 <daffamantap29@gmail.com>
2025-04-14 02:39:35 +0700

    first commits :)

---
+++ tp_mod_8_103022300056/CovidConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public double batas_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    private static string filePath = "covid_config.json";
+
+    public static CovidConfig LoadConfig()
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<CovidConfig>(jsonString);
+        }
+        else
+        {
+            CovidConfig defaultConfig = new CovidConfig
+            {
+                satuan_suhu = "celsius",
+                batas_demam = 37.5,
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                pesan_diterima = "Anda dipersilakan untuk masuk ke dalam gedung ini"
+            };
+
+            defaultConfig.SaveConfig();
+            return defaultConfig;
+        }
+    }
+
+    public void SaveConfig()
+    {
+        string jsonString = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, jsonString);
+    }
+
+    public void UbahSatuan()
+    {
+        if (satuan_suhu.ToLower() == "celsius")
+        {
+            satuan_suhu = "fahrenheit";
+            batas_demam = Math.Round((batas_demam * 9 / 5) + 32, 1);
+        }
+        else
+        {
+            satuan_suhu = "celsius";
+            batas_demam = Math.Round((batas_demam - 32) * 5 / 9, 1);
+        }
+        SaveConfig();
+    }
+}
---
+++ tp_mod_8_103022300056/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config = CovidConfig.LoadConfig();
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? (dalam satuan {config.satuan_suhu})");
+        double suhu = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Berapa jarak anda ke lokasi (perkiraan meter)?");
+        double jarak = double.Parse(Console.ReadLine());
+
+        bool suhuAman = suhu <= config.batas_demam;
+        bool jarakAman = jarak <= 1.5;
+
+        if (suhuAman && jarakAman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        Console.WriteLine("\nApakah anda ingin mengubah satuan suhu? (y/n)");
+        string jawab = Console.ReadLine();
+
+        if (jawab.ToLower() == "y")
+        {
+            config.UbahSatuan();
+            Console.WriteLine("Satuan suhu berhasil diubah!");
+        }
+    }
+}
+
